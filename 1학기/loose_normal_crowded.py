97f234a92bbadddb46cd167fa6a3828591753041
Jeong Yugyeong <dbruddl2002@naver.com>
2019-09-10 11:52:36 +0900

    여러 정거장 반복하여 승객수 누적

--- 1학기/loose_normal_crowded.py
+++ 1학기/loose_normal_crowded.py
@@ -3,11 +3,18 @@
 # 0 이상 10 미만 : 여유
 # 10 이상 20 미만 : 보통
 # 20 이상 : 혼잡
-in0 = input("탑승객 수는 ?")
-in0 = int(in0)
-out = input("하차객 수는 ?")
-out = int(out)
-sum = in0 - out
+# -----
+sum = 0
+while True:
+    in0 = input("탑승객 수는 ? (-1:끝)")
+    if in0=="-1":
+        break
+    in0 = int(in0)
+    out = input("하차객 수는 ?")
+    out = int(out)
+    sum = in0 - out # 이 값을 계속 sum에 더해줘야 누적된다
+# ----- 이만큼 계속 돌리겠다
+
 print("버스에 있는 사람수는 ?", sum)
 
 if 0 <= sum <10:
