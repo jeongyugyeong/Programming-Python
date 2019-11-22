363451fdd65c539edfa0af96b7a6f3ed1d8779b7
Jeong Yugyeong <dbruddl2002@naver.com>
2019-11-22 14:55:32 +0900

    web crawling - 유미의 세포들 -> html로 저장하자

--- 1학기/프로젝트/game_manager.py
+++ 1학기/프로젝트/game_manager.py
@@ -1,4 +1,6 @@
 from tictactoe import TicTacToe
+import tkinter
+import math
 
 class GameManager:
     def __init__(self):
@@ -27,6 +29,55 @@ class GameManager:
                 print("무승부")
                 break
 
+class GameManager_GUI :
+    def __init__(self):
+        self.ttt = TicTacToe()
+        CANVAS_SIZE = 300
+        self.TILE_SIZE = CANVAS_SIZE/3
+
+        self.root = tkinter.Tk()
+        self.root.title("틱 택 토")
+        self.root.geometry(str(CANVAS_SIZE)+"x"+str(CANVAS_SIZE))  #geometry("300x300")
+        self.root.resizable(width=False, height=False)
+        self.canvas = tkinter.Canvas(self.root, bg="white", width=CANVAS_SIZE, height=CANVAS_SIZE)
+        self.canvas.pack()
+
+        self.images = dict()
+        self.images["O"] = tkinter.PhotoImage(file="img/O.gif")
+        self.images["X"] = tkinter.PhotoImage(file="img/X.gif")
+        self.canvas.bind("<Button-1>", self.click_handler)
+
+    def click_handler(self, event):
+        row = math.floor(event.y/self.TILE_SIZE)
+        col = math.floor(event.x/self.TILE_SIZE)
+
+
+    def draw_board(self):
+        #clear
+        self.canvas.delete("all")
+
+        SIZE = 100
+        x = 0
+        y = 0
+        for i, v in enumerate(self.ttt.board):
+            if v == ".":
+                pass
+            elif v == "O":
+                self.canvas.create_image(x, y, anchor="nw", images=self.images["O"])
+            elif v == "X":
+                self.canvas.create_image(x, y, anchor="nw", images=self.images["X"])
+            x += self.TILE_SIZE
+            if i % 3 == 2:
+                x = 0
+                y += self.TILE_SIZE
+
+    def play(self):
+        self.root.mainloop()
+
+
+
 if __name__ == '__main__':
-    gm = GameManager()
+    gm = GameManager_GUI()
     gm.play()
+    gm.ttt.set(1, 1)
+    gm.draw_board()
--- 1학기/프로젝트/web_crawling.py
+++ 1학기/프로젝트/web_crawling.py
@@ -2,13 +2,23 @@ from bs4 import BeautifulSoup #html 구조적으로 변환하자
 from urllib.request import urlopen #url에 해당하는 html 가져오자
 
 if __name__ == '__main__':
-    data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=700844")
+    data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=651673")
     soup = BeautifulSoup(data, "lxml")
     print(soup)
-    cartoon_titles = soup.find_all("td", attrs={'class':'title'})
+    cartoon_titles = soup.find_all("td", attrs={'class':'title'})   #<td class="title>
+    html = "<html><head><meta charset='utf-8'></head></body>"
     for title in cartoon_titles:
-        t = title.find('a').text
-        print(t)
+        t = title.find('a').text   #제목 가져오자
+        link = title.find('a').get("href")
+        link = "http://comic.naver.com/"+link
+        # print(t)
+        # print(link)
+        html+="<a href='"+link+"'>"+t+"</a><br><br>"
+    html+="</body></html>"
+    outputSoup = BeautifulSoup(html, "lxml")  #htmlstring -> Beautiful Soup 객체
+    prettyHtml = str(outputSoup.prettify())   #html 줄 예쁘게
+    with open("유미의세포들.html", "w", encoding="utf-8")as f:
+        f.write(prettyHtml)
 
 # data = urlopen("http://webtoon.daum.net/webtoon/view/findjuly")
 # soup = BeautifulSoup(data, "lxml")
