a82f34e98ed8837d9af8d3898d30f87a3bfbd2ad
Jeong Yugyeong <dbruddl2002@naver.com>
2019-11-29 13:51:41 +0900

    다음 영화 박스오피스

--- 1학기/프로젝트/web_crawling.py
+++ 1학기/프로젝트/web_crawling.py
@@ -2,9 +2,14 @@ from bs4 import BeautifulSoup #html 구조적으로 변환하자
 from urllib.request import urlopen #url에 해당하는 html 가져오자
 
 if __name__ == '__main__':
-    data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=651673")
-    soup = BeautifulSoup(data, "lxml")
-    print(soup)
+    # data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=651673")
+    # soup = BeautifulSoup(data, "lxml")
+    # data.close()
+
+    with urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=651673") as data:
+        soup = BeautifulSoup(data, "lxml")
+
+    # print(soup)
     cartoon_titles = soup.find_all("td", attrs={'class':'title'})   #<td class="title>
     html = "<html><head><meta charset='utf-8'></head></body>"
     for title in cartoon_titles:
